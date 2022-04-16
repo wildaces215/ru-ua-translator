90848931c6acd04956bdbf90bce3a93b9ac32bff
wildaces215 <test@test.com>
2022-04-16 15:32:55 -0400

    fixed ui layout

--- main.py
+++ main.py
@@ -2,10 +2,12 @@
 
 from argostranslate import package, translate
 from PyQt5.QtWidgets import QApplication, QWidget,QLabel,QGridLayout,QTextEdit,QPushButton
+from PyQt5.QtCore import pyqtSlot
+
 import sys
 
 #Local Imports
-#from translate import RuTranslate
+from translate import Translator
 
 class MainScreen(QWidget):
     def __init__(self):
@@ -23,7 +25,7 @@ class MainScreen(QWidget):
 
         self.leftTextBox = QTextEdit(self)
         self.rightTextBox = QTextEdit(self) 
-
+        
         self.translateButton = QPushButton("Translate")
         
         self.createDBButton = QPushButton("Create Database")
@@ -31,19 +33,32 @@ class MainScreen(QWidget):
         self.exportDatabase = QPushButton("Export Database")
 
         grid.addWidget(self.leftTextBox,0,0)
-        grid.addWidget(self.rightTextBox,0,1)
+        grid.addWidget(self.translateButton,0,1)
+        grid.addWidget(self.rightTextBox,0,2)
 
        
         grid.addWidget(self.addToDatabase,1,0)
         grid.addWidget(self.exportDatabase,1,1)
 
-
+        #Actions
+        self.translateButton.clicked.connect(self.translate_action)
+        self.addToDatabase.clicked.connect(self.add_to_db)
         
-       
         self.show()
+   
+    @pyqtSlot()
+    def translate_action(self):
+        original_text = self.leftTextBox.toPlainText()
+        translator = Translator()
+        print(original_text)
         
+    @pyqtSlot()
+    def add_to_db(self):
+        print("The add to db button was called.")
     
-
+    @pyqtSlot()
+    def export_db(self):
+        print("The export to db was called!")
 
 
 def main():
